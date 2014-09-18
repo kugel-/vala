kugel-/vala

commit ba1fa0759989dcbb9046c7dfc06cce6c4aa23411

Fix regression when assigning owned expressions to unowned variables.
The regression was introduced by db462e6f919da758fb.

Fixes bug 736016

vala/valasemanticanalyzer.vala
@@ -892,8 +892,7 @@ public class Vala.SemanticAnalyzer : CodeVisitor {
 	public static Expression create_temp_access (LocalVariable local, DataType? target_type) {
 		Expression temp_access = new MemberAccess.simple (local.name, local.source_reference);
 
-		var target_owned = target_type == null || target_type.value_owned;
-		if (target_owned && local.variable_type.is_disposable ()) {
+		if (local.variable_type.is_disposable ()) {
 			temp_access = new ReferenceTransferExpression (temp_access, local.source_reference);
 			temp_access.target_type = target_type != null ? target_type.copy () : local.variable_type.copy ();
 			temp_access.target_type.value_owned = true;

commit e8915408926541584c77a1b914327664b80f0372

Fix regression introduced by ba1fa07
Hope this is the last time we touch this code.
In the owned->unowned case, we want the variable to be floating:
 - If we assign to a local variable we get an error.
 - Otherwise the value is destroyed when goes out of scope.
In the owned->owned case, we want to avoid copies and thus
we transfer the ownership and make the variable non-floating.

Fixes bug 736774

vala/valabinaryexpression.vala
@@ -233,6 +233,9 @@ public class Vala.BinaryExpression : Expression {
 			insert_statement (context.analyzer.insert_block, decl);
 			insert_statement (context.analyzer.insert_block, if_stmt);
 
+			// create before check as local.floating might become false
+			var temp_access = SemanticAnalyzer.create_temp_access (local, target_type);
+			
 			if (!decl.check (context)) {
 				error = true;
 				return false;
@@ -243,7 +246,6 @@ public class Vala.BinaryExpression : Expression {
 				return false;
 			}
 
-			var temp_access = SemanticAnalyzer.create_temp_access (local, target_type);
 			temp_access.check (context);
 
 			parent_node.replace_expression (this, temp_access);

vala/valamethodcall.vala
@@ -638,6 +638,7 @@ public class Vala.MethodCall : Expression {
 
 				insert_statement (context.analyzer.insert_block, decl);
 
+				// create before check as local.floating might become false
 				var temp_access = SemanticAnalyzer.create_temp_access (local, target_type);
 
 				// don't set initializer earlier as this changes parent_node and parent_statement

vala/valaobjectcreationexpression.vala
@@ -435,11 +435,11 @@ public class Vala.ObjectCreationExpression : Expression {
 
 				insert_statement (context.analyzer.insert_block, decl);
 
+				// create before check as local.floating might become false
 				var temp_access = SemanticAnalyzer.create_temp_access (local, target_type);
 				// don't set initializer earlier as this changes parent_node and parent_statement
 				local.initializer = this;
 				decl.check (context);
-
 
 				// move temp variable to insert block to ensure the
 				// variable is in the same block as the declaration

vala/valasemanticanalyzer.vala
@@ -897,6 +897,7 @@ public class Vala.SemanticAnalyzer : CodeVisitor {
 			temp_access = new ReferenceTransferExpression (temp_access, local.source_reference);
 			temp_access.target_type = target_type != null ? target_type.copy () : local.variable_type.copy ();
 			temp_access.target_type.value_owned = true;
+			local.floating = false;
 		} else {
 			temp_access.target_type = target_type != null ? target_type.copy () : null;
 		}

tests/Makefile.am
@@ -75,6 +75,7 @@ TESTS = \
 	control-flow/bug661985.vala \
 	control-flow/bug665904.vala \
 	control-flow/bug691514.vala	\
+	control-flow/bug736774.vala \
 	enums/enums.vala \
 	enums/flags.vala \
 	enums/bug673879.vala \

tests/control-flow/bug736774.vala
@@ -1,0 +1,23 @@
+bool destroyed = false;
+
+class Foo : Object {
+    ~Foo() {
+		destroyed = true;
+    }
+}
+
+Foo may_fail () throws Error {
+	return new Foo ();
+}
+
+void func (Foo x) {
+}
+
+void main() {
+    try {
+        func (may_fail ());
+    } catch {
+    }
+
+	assert (destroyed);
+}